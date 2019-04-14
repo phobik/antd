71f3a0e5ecb69ac2fcd76e646ee41c7fae262e39
phob <phobik2000@gmail.com>
2019-04-14 23:59:17 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Button from 'antd/lib/button';
-
 import './App.css';
 
 class App extends Component {
@@ -11,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-
+            Edit the code in test <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >Learn React code
+          >
+            Learn React
           </a>
         </header>
       </div>
